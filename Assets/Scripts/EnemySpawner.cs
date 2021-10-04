16c865047bb3abf82a684a2db7559c79ac087fb1
Kenric D'Souza <AzureByte@users.noreply.github.com>
2021-10-04 18:52:55 +0530

    Falling rocks, No 3d physics and connected the life meter

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject EnemyPrefab;
+    public Sprite[] EnemyTypes;
+    public Color[] colours;
+
+    private float _spawnInterval = 4f;
+
+    //Make it harder as the game goes on but not impossible.
+    private float _spawnIntervalMin = 1f;
+    private float _lastSpawnTime = 0f;
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Time.time > _lastSpawnTime + _spawnInterval)
+        {
+            _lastSpawnTime = Time.time;
+            Vector3 spawnPos = transform.position;
+            spawnPos.x = Random.Range(-4f, 4f);
+            GameObject Enemy = Instantiate(EnemyPrefab, spawnPos, Quaternion.identity);
+
+            Enemy.GetComponent<SpriteRenderer>().sprite = EnemyTypes[Random.Range(0, EnemyTypes.Length)];
+        }
     }
 }
